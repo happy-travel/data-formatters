5648565abbfb6c8916ae5dd9e5e13f626f1b5794
Hasmik <hasmik_harutyunyan@outlook.com>
2020-10-20 14:52:30 +0400

    deleted files to empty default branch

--- HappyTravel.Formatters/HappyTravel.Formatters/DateTimeFormatters.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace HappyTravel.Formatters
-{
-    public static class DateTimeFormatters
-    {
-        public static string FromDateTime(DateTime dateTime) => $"{dateTime:yyyy.MM.dd hh:mm} UTC";
-    }
-}
