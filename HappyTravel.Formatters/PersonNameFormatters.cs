1d86c8300d01428fc6b1f1725e4c7c6b0d7711f5
Denis Nikolaev <dfs.str@gmail.com>
2021-01-13 14:30:23 +0500

    fixed NullReferenceException in PersonNameFormatters

--- HappyTravel.Formatters/PersonNameFormatters.cs
+++ HappyTravel.Formatters/PersonNameFormatters.cs
@@ -4,7 +4,11 @@ namespace HappyTravel.Formatters
     {
         public static string ToMaskedName(string firstName, string lastName, string? title = null)
         {
-            var mask = firstName.Length == 1 ? "*" : firstName.Substring(0, 1);
+            var mask = firstName != null
+                ? firstName.Length == 1
+                    ? "*"
+                    : firstName.Substring(0, 1)
+                : string.Empty;
 
             return title is null
                 ? $"{mask}. {lastName}"
